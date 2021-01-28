223c8a75dff23aa297e6ca9c34a338b6914e2db3
jefflin <linjef0711@gmail.com>
2021-01-28 11:47:05 +0800

    feat : Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
 const port = 3000
+
+app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
