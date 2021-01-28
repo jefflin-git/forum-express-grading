629253f966ca1d05a7b0bd9b3e726411525ee6fb
jefflin <linjef0711@gmail.com>
2021-01-28 15:10:21 +0800

    feat : add restaurant index page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const handlebars = require('express-handlebars')
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
