8ead9147aebb7b2780cfb8b55cbd884d516f9a3b
jefflin <linjef0711@gmail.com>
2021-01-28 20:30:32 +0800

    feat : add userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()
