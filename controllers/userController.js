196bc52344ddff864aea1af1736d162edfdaf1f9
jefflin <linjef0711@gmail.com>
2021-01-29 11:06:29 +0800

    feat : add signIn page, signIn and logout feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,6 +28,17 @@ const userController = {
           return res.redirect('/signin')
         })
       })
+  },
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
+const passport = require('passport')
+
 module.exports = (app) => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
@@ -11,4 +13,12 @@ module.exports = (app) => {
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
+
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', {
+    successRedirect: '/restaurants',
+    failureRedirect: '/signin'
+  }), userController.signIn)
+
+  app.get('/logout', userController.logout)
 }
