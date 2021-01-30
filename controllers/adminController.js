a8187e7ab69155bddf3735c7db04303d54bd93fb
jefflin <linjef0711@gmail.com>
2021-01-30 18:32:15 +0800

    feat : install multer and add image upload feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
+const fs = require('fs')
 
 const adminController = {
   getRestaurants: (req, res) => {
@@ -17,17 +18,39 @@ const adminController = {
       req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    Restaurant.create({
-      name,
-      tel,
-      address,
-      opening_hours,
-      description
-    })
-      .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created')
-        res.redirect('/admin/restaurants')
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          Restaurant.create({
+            name,
+            tel,
+            address,
+            opening_hours,
+            description,
+            image: file ? `/upload/${file.originalname}` : null
+          })
+            .then(() => {
+              req.flash('success_messages', 'restaurant was successfully created')
+              return res.redirect('/admin/restaurants')
+            })
+        })
       })
+    } else {
+      Restaurant.create({
+        name,
+        tel,
+        address,
+        opening_hours,
+        description,
+        image: null
+      })
+        .then(() => {
+          req.flash('success_messages', 'restaurant was successfully created')
+          return res.redirect('/admin/restaurants')
+        })
+    }
   },
   getRestaurant: (req, res) => {
     const id = req.params.id
@@ -54,20 +77,39 @@ const adminController = {
       req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    Restaurant.findByPk(id)
-      .then(restaurant => {
-        restaurant.update({
-          name,
-          tel,
-          address,
-          opening_hours,
-          description
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          Restaurant.create({
+            name,
+            tel,
+            address,
+            opening_hours,
+            description,
+            image: file ? `/upload/${file.originalname}` : restaurant.image
+          })
+            .then(() => {
+              req.flash('success_messages', 'restaurant was successfully created')
+              return res.redirect('/admin/restaurants')
+            })
         })
       })
-      .then(() => {
-        req.flash('success_messages', 'restaurant was successfully to update')
-        res.redirect('/admin/restaurants')
+    } else {
+      Restaurant.create({
+        name,
+        tel,
+        address,
+        opening_hours,
+        description,
+        image: restaurant.image
       })
+        .then(() => {
+          req.flash('success_messages', 'restaurant was successfully created')
+          return res.redirect('/admin/restaurants')
+        })
+    }
   },
   deleteRestaurant: (req, res) => {
     const id = req.params.id
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const userController = require('../controllers/userController')
 
 const passport = require('passport')
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 module.exports = (app) => {
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) return next()
@@ -27,12 +30,12 @@ module.exports = (app) => {
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
-  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
 
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
 
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
-  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
 
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
