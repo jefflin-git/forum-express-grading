ef6a5cc295b50a7679c4a94f01f688020f98b949
jefflin <linjef0711@gmail.com>
2021-02-20 21:36:34 +0800

    feat : add topUser page

--- routes/index.js
+++ routes/index.js
@@ -58,6 +58,7 @@ module.exports = (app) => {
 
   app.get('/logout', userController.logout)
 
+  app.get('/users/top', authenticated, userController.getTopUser)
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
   app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
